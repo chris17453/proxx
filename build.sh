313611c4f0e2dc7ca843a568c3330b174d631c86
Chris W <nd@atl1macods1.local>
2018-11-02 16:11:55 -0400

    Bump Version

--- build.sh
+++ build.sh
@@ -3,22 +3,52 @@ dir=$(pwd)
 pub=$1
 
 
+if [[ $PIPENV_ACTIVE -ne '1' ]];
+then
+    if [[ $BUILD_O -ne 1 ]];
+    then
+        export BUILD_O=1
+        echo "Going Deeper  -> pipenv shell"
+        pipenv run ./build.sh
+        exit 0
+    fi
+fi
+
+
+
 echo "Attempting to PyPi package"
 
 echo "Remove old images"
-mkdir dist
+if [[ ! -d 'dist' ]];
+then
+    mkdir dist
+fi
+
 cd dist
 rm *.gz -f
 cd ..
 
+if [[ ! -d '.git' ]];
+then
+    git init
+fi
 echo "Adding git changes"
 git add -A 
+git commit -m 'Bump Version'
 
 echo "Bumping Python patch version"
 bumpversion patch --allow-dirty
 if [[ $? -ne 0 ]]; then
-    echo "in the wrong env?"
-    exit 1
+    pipenv install bumpbversion
+    
+echo [bumpversion] \
+current_version = 1.0.0 \
+files = setup.py \
+commit = False \
+tag = True >.bumpversion
+git add .bumpversion
+git commit -m 'BumpVersion Config'
+
 fi
 
 echo "Build the package"
@@ -30,7 +60,10 @@ if [[ ! -z "$pub" ]]; then
     twine upload  dist/*
 fi
 
-mkdir test
+if [[ ! -d 'test' ]];
+then
+    mkdir test
+fi
 
 echo "Updating test Environment"
 cd test
